3c3bc1e4fd9d2c93cf4a6cc01501fdd73bd9444c
Fedor Kostrov <kostrov.f.dev@gmail.com>
2025-02-05 12:38:28 +0300

    update + delete user

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ type Role uint8
 
 const (
 	// AdminRole is ...
-	AdminRole Role = iota
+	AdminRole Role = iota + 1
 	// UserRole is ...
 	UserRole
 )
@@ -47,6 +47,14 @@ type UserData struct {
 	Role      Role      `json:"role"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+// UpdateUserData is ...
+type UpdateUserData struct {
+	ID    int64  `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Role  Role   `json:"role"`
 }
 
 func createUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -82,7 +90,7 @@ func getUserHandler(w http.ResponseWriter, r *http.Request) {
 	userIDStr := chi.URLParam(r, "id")
 	userID, err := parseID(userIDStr)
 	if err != nil {
-		http.Error(w, "Invalid note ID", http.StatusBadRequest)
+		http.Error(w, "Invalid user ID", http.StatusBadRequest)
 		return
 	}
 
@@ -113,10 +121,48 @@ func parseID(idStr string) (int64, error) {
 	return id, nil
 }
 
+func updateUserHandler(w http.ResponseWriter, r *http.Request) {
+	updatedUser := &UpdateUserData{}
+	userIDStr := chi.URLParam(r, "id")
+	userID, err := parseID(userIDStr)
+	if err != nil {
+		http.Error(w, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+	if err := json.NewDecoder(r.Body).Decode(updatedUser); err != nil {
+		http.Error(w, "Failed to decode new user data", http.StatusBadRequest)
+		return
+	}
+	updatedUser.ID = userID
+
+	updateUser(updatedUser)
+}
+
+func updateUser(user *UpdateUserData) {
+	fmt.Printf("update user data: %+v\n", *user)
+}
+
+func deleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	userIDStr := chi.URLParam(r, "id")
+	userID, err := parseID(userIDStr)
+	if err != nil {
+		http.Error(w, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+
+	deleteUser(userID)
+}
+
+func deleteUser(id int64) {
+	fmt.Printf("delete user id: %v\n", id)
+}
+
 func main() {
 	r := chi.NewRouter()
 	r.Post(usersPostfix, createUserHandler)
 	r.Get(userPostfix, getUserHandler)
+	r.Put(userPostfix, updateUserHandler)
+	r.Delete(userPostfix, deleteUserHandler)
 
 	server := http.Server{
 		Addr:         baseURL,
